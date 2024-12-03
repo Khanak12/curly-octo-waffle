451dc568660c4ed23d494bf2bf3ee2fa52a6c9d1
Khanak12 <khanakjoshi2012@gmail.com>
2024-12-02 19:21:44 -0500

    Add files via upload

---
+++ main33.py
@@ -0,0 +1,13 @@
+class a:
+    x="Bird"
+    def __init__(self,name,color):
+        self.a=name
+        self.b=color
+        
+obj=a("Parrot","red")
+print(obj.a)
+print(obj.b)
+print(obj.x)
+obj1=a("Crow","Black")
+print(obj1.a)
+print(obj1.b)
